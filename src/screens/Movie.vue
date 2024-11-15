56e630697c2d55fee5e549efb690693bf37b7d5d
Nuno Gomes <nuno98gomes@gmail.com>
2024-11-15 21:08:51 +0100

    [fix] fixed update/delete actions

--- src/screens/Movie.vue
+++ src/screens/Movie.vue
@@ -82,7 +82,8 @@ async function handleOnSubmit() {
       doc(db, 'movies', movieData.value.id),
       movieData.value
     ).then(() => {
-      router.push('/');
+      isEditing.value = false;
+      window.location.reload();
     });
   } catch (error) {
     console.error('!! Error adding/updating:', error);
@@ -98,7 +99,7 @@ async function handleOnDelete() {
 
   try {
     await deleteDoc(doc(db, 'movies', movieData.value.id)).then(() => {
-      router.push('/');
+      window.location.reload();
     });
   } catch (error) {
     console.error('!! Error deleting:', error);
