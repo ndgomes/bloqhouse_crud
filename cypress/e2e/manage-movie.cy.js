131809aeacb2f20ab0556edc052d69556bb87b44
Nuno Gomes <nuno98gomes@gmail.com>
2024-11-17 15:54:20 +0100

    refactor e2e test

--- cypress/e2e/manage-movie.cy.js
+++ cypress/e2e/manage-movie.cy.js
@@ -1,50 +1,57 @@
-describe('Add movie', () => {
+Cypress.Commands.add(
+  'addMovie',
+  (title, description, genre, rating, releaseYear, coverImage) => {
+    cy.get('[data-testid="add-movie"]').click();
+    cy.get('input[name="title"]').type(title);
+    cy.get('textarea[name="description"]').type(description);
+    cy.get(`input[name="genre"][value="${genre}"]`).check();
+    cy.get('input[name="rating"]').type(rating);
+    cy.get('input[name="releaseYear"]').type(releaseYear);
+    cy.get('input[name="coverImage"]').type(coverImage);
+    cy.get('[data-testid="submit-movie"]').click();
+  }
+);
+
+Cypress.Commands.add('editMovie', (title, newReleaseYear) => {
+  cy.get('h3[class="title"]').contains(title).click();
+  cy.get('[data-testid="edit-movie"]').click();
+  cy.get('input[name="releaseYear"]').clear().type(newReleaseYear);
+  cy.get('[data-testid="confirm-edit-movie"]').contains('Save Changes').click();
+});
+
+Cypress.Commands.add('deleteMovie', (title) => {
+  cy.get('h3[class="title"]').contains(title).click();
+  cy.get('[data-testid="delete-movie"]').click();
+  cy.get('[data-testid="confirm-delete-movie"]').contains('Yes').click();
+});
+
+describe('Add/Edit/Delete Movie', () => {
+  const uniqueTitle = `The Matrix ${Date.now()}`;
+
   beforeEach(() => {
     cy.visit('/');
   });
 
-  // Test for adding a movie
   it('should add a movie', () => {
-    cy.get('[data-testid="add-movie"]').contains('Add Movie').click();
-    cy.get('input[name="title"]').type('The Matrix');
-    cy.get('textarea[name="description"]').type(
-      'A movie about a man who fights crime.'
-    );
-    cy.get('input[name="genre"][value="Action"]').check();
-    cy.get('input[name="rating"]').type('8');
-    cy.get('input[name="releaseYear"]').type('1999');
-    cy.get('input[name="coverImage"]').type(
+    cy.addMovie(
+      uniqueTitle,
+      'A movie about a man who fights crime.',
+      'Action',
+      '8',
+      '1999',
       'https://image.tmdb.org/t/p/w500/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg'
     );
-    cy.get('[data-testid="submit-movie"]').click();
 
-    cy.wait(3000);
-
-    cy.get('h3[class="title"]').contains('The Matrix').should('exist');
+    cy.get('h3[class="title"]').contains(uniqueTitle).should('exist');
   });
 
-  // Test for editing a movie
   it('should edit a movie', () => {
-    cy.get('h3[class="title"]').contains('The Matrix').click();
-    cy.get('[data-testid="edit-movie"]').click();
-    cy.get('input[name="releaseYear"]').clear().type('2000');
-    cy.get('[data-testid="confirm-edit-movie"]')
-      .contains('Save Changes')
-      .click();
-
-    cy.wait(3000);
-
+    cy.editMovie(uniqueTitle, '2000');
     cy.get('p').contains('Release Year: 2000').should('exist');
   });
 
-  // Test for deleting a movie
   it('should delete a movie', () => {
-    cy.get('h3[class="title"]').contains('The Matrix').click();
-    cy.get('[data-testid="delete-movie"]').click();
-    cy.get('[data-testid="confirm-delete-movie"]').contains('Yes').click();
-
-    cy.wait(3000);
-
-    cy.get('h3[class="title"]').contains('The Matrix').should('not.exist');
+    cy.deleteMovie(uniqueTitle);
+    cy.get('h3[class="title"]').contains(uniqueTitle).should('not.exist');
   });
 });
